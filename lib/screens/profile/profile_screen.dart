02c240128bfb5114ae66a98b81ae7465248da3af
Faysal <faysalneowaz@gmail.com>
2022-09-29 01:45:20 +0600

    half done profile screen

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+
 import 'package:schoolerp/constant.dart';
 import 'package:schoolerp/screens/profile/bodycontent/top_profile.dart';
 
@@ -65,8 +67,147 @@ class _ProfileScreenState extends State<ProfileScreen> {
             child: Column(
               children: [
                 const TopProfile(),
+                Row(
+                  children: [
+                    Flexible(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 5),
+                        child: TextFieldCustom(
+                          labeltext: "Birth Certificate No",
+                          hinttext: "1234 4325 4567 1234",
+                          isLocked: false,
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 5),
+                        child: TextFieldCustom(
+                          labeltext: "Academic Year",
+                          hinttext: "2021-2022",
+                          isLocked: false,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 5),
+                        child: TextFieldCustom(
+                          labeltext: "Admission Class",
+                          hinttext: "VI",
+                          isLocked: true,
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 5),
+                        child: TextFieldCustom(
+                          labeltext: "Old Admission No",
+                          hinttext: "T0022",
+                          isLocked: true,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 5),
+                        child: TextFieldCustom(
+                          labeltext: "Date of Admission",
+                          hinttext: "101 Apr 2018",
+                          isLocked: true,
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 5),
+                        child: TextFieldCustom(
+                          labeltext: "Date of Birth",
+                          hinttext: "22 July 1996",
+                          isLocked: true,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 5),
+                        child: TextFieldCustom(
+                          labeltext: "Parent Mail ID",
+                          hinttext: "parentboth@gmail.com",
+                          isLocked: true,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextFieldCustom extends StatelessWidget {
+  String? labeltext;
+  String? hinttext;
+  bool isLocked;
+  TextFieldCustom({
+    Key? key,
+    required this.labeltext,
+    required this.hinttext,
+    required this.isLocked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      cursorColor: AppConstent.lightgraycolor,
+      decoration: InputDecoration(
+        labelText: labeltext,
+        hintText: hinttext,
+        suffixIcon: isLocked
+            ? const Icon(
+                Icons.lock_outlined,
+                color: AppConstent.lightgraycolor,
+              )
+            : null,
+        suffixIconColor: AppConstent.lightgraycolor,
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        labelStyle: const TextStyle(
+          color: AppConstent.lightgraycolor,
+        ),
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: AppConstent.lightgraycolor,
+          ),
+        ),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: AppConstent.lightgraycolor,
           ),
         ),
       ),
